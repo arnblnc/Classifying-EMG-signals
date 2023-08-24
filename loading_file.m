7c70f7d47f5a1503744820b5ed00363fee65d0ee
Aaron <102743367+kakashim0t0@users.noreply.github.com>
2023-08-24 08:49:41 +0100

    loading_file.m

--- loading_file.m
+++ loading_file.m
@@ -1,5 +1,5 @@
 % Load the mat file
-loaded_data = load('/Users/Aaron/Library/CloudStorage/OneDrive-Personal/Documents/Control Robotic Arm/Data/C008_20130528_072811_072820.mat');
+loaded_data = load('Replace with path of data');
 
 % Extract signal and time data
 data = loaded_data.daq.DAQ_DATA;
